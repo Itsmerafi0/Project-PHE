f4ae250c4a4402075ebfb5fdf13ec017b27d1bf4
Rafi Azhar <itsmerafi0@gmail.com>
2023-12-30 14:19:52 +0700

    Update Migrations

---
+++ Client/Repositories/Data/VendorRepository.cs
@@ -0,0 +1,20 @@
+﻿using Client.Models;
+using Client.Repositories.Interface;
+
+namespace Client.Repositories.Data
+{
+    public class VendorRepository : GeneralRepository<Vendor, Guid>, IVendorRepository
+    {
+        private readonly HttpClient httpClient;
+        private readonly string request;
+
+        public VendorRepository(string  request = "ticket/") :base (request)
+        {
+            this.request = request;
+            httpClient = new HttpClient { BaseAddress = new Uri("https://localhost:7023:/api/") 
+            };
+        }
+
+
+    }
+}
---
+++ Client/ViewModels/GetApprovelVM.cs
@@ -0,0 +1,8 @@
+﻿namespace Client.ViewModels
+{
+    public class GetApprovelVM
+    {
+        public string Guid { get; set; }
+        public string ApprovalVendor { get; set; }
+    }
+}
